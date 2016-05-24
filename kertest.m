9be5d4cce738be3e2422fe5bc47989dc2e9bdc83
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-24 20:57:38 +0300

    Update kertest.m

--- kertest.m
+++ kertest.m
@@ -5,5 +5,13 @@ for i=1:L^2
     [k, j] = kernel( k0, i, j ) ;
     kk = [kk; k];
 end
-imagesc(kk)
+figure(1), imagesc(kk)
+title('Interaction Kernel')
+[D, L] = eig( kk );
+figure(2), plot(L)
+title('Eignevalue Spectrum')
+figure(3), imagesc(real(D))
+title('Eigenvector matrix (Real Part)')
+figure(4), imagesc(real(D))
+title('Eigenvector matrix (Imag. Part)')
 end
