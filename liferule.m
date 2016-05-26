d9f12bfb0f9a43fea8e38e815311feb6907c0513
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-26 18:39:55 +0300

    Update liferule.m

--- liferule.m
+++ liferule.m
@@ -4,7 +4,8 @@ for i=1:9
     s = [s, s+1];
 end
 % Use ternary encoding for additive rule using transitions only 
-% 1st half: 0 -> 1, 2nd half: 1 -> 0
+% 1st half: 0 -> 1, living neighbors 3
+% 2nd half: 1 -> 0, living neighbors + central cell 2 or 4
 half = 2^8; 
-R = [ (s(1:half)==4), - (s(half+1:end)==2) - (s(half+1:end)>4) ];
+R = [ (s(1:half)==3), - (s(half+1:end)==2) - (s(half+1:end)>4) ];
 end
