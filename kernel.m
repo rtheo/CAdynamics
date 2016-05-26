a63c8630ebadfa9fdff1868ebc4afdb369e2b847
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-26 17:45:00 +0300

    Update and rename kertest.m to kernel.m

--- kernel.m
+++ kernel.m
@@ -1,10 +1,12 @@
-function kertest(L)
+function kernel(L, plt)
+if nargin<2, plt=0; end
 k0 = kerinit(L);   % initialize kernel sub-vectors
 j = 1; kk = [];
 for i=1:L^2
     [k, j] = kernel( k0, i, j ) ;
     kk = [kk; k];
 end
+if plt
 figure(1), imagesc(kk)
 title('Interaction Kernel')
 [D, E] = eig( kk ); x = 1:L^2;
@@ -14,4 +16,5 @@ figure(3), imagesc(real(D))
 title('Eigenvector matrix (Real Part)')
 figure(4), imagesc(real(D))
 title('Eigenvector matrix (Imag. Part)')
+end
 end
