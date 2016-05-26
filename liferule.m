b61f3e58c8c4d1df6723e1fa87750a396f7e1672
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-26 17:46:07 +0300

    Update liferule.m

--- liferule.m
+++ liferule.m
@@ -1,11 +1,10 @@
-function [R, s1, s2 ] = liferule
-s1 = 0; % create digit-sum sequence for all 9-bit strings in lexicographic order
-% left half-interval (central cell dead.)
-for i=1:8
-    s1 = [s1, s1+1];
+function R  = liferule  %Problem with this rule -- to be chekced! --
+s = 0; % create digit-sum sequence for all 9-bit strings in lexicographic order
+for i=1:9
+    s = [s, s+1];
 end
-half = 2^8;
- s = [s1, s1+1];s2 = s(half+1:end); % right half-interval (central cell alive.)
- % define output values for living central cells in the two half-intervals
-R = [(s1==3), (s2 == 3) + (s2 == 4)]; 
+% Use ternary encoding for additive rule using transitions only 
+% 1st half: 0 -> 1, 2nd half: 1 -> 0
+half = 2^8; 
+R = [ (s(1:half)==3), - (s(half+1:end)==1) - (s(half+1:end)>3) ];
 end
