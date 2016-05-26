fe37c2182e6f41323f769447fd16054e1452947f
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-26 17:48:31 +0300

    Update liveon.m

--- liveon.m
+++ liveon.m
@@ -1,26 +1,37 @@
-function liveon(v, time, graf)
+function liveon( c, steps, graph )
 % Example with direct application of matrix convolution
-% try it with the simplest blinker 2-period oscillator 
-% c = [zeros(1,6), 1 1 1, zeros(1, 16)]; 
+% Try it with liveon( [], ... )
 clc, close all
-L = sqrt( length(v) );
-if mod(L, 1)>0, error('In. Cond. length not a square number!'), end
+if isempty(c), 
+    disp(' Trial Patterns \n');
+    disp('(1) 2-period blinker')
+    disp('(2) 5-period octagon')
+    disp('(3) R-Pentomino "Methuselah" ')
+    in=input('Enter your choice: \n');
+    switch in
+        case 1, c = [zeros(1, 11), 1 1 1, zeros(1, 11)];
+        case 2, w = zeros(4); for i=1:4, w( i, 4 - i + 1) = 1; end
+                    c = [ w, fliplr( w ) ]; c = [ c; flipud( c ) ]; c = reshape(c, 1, 64);
+        case 3, c = [zeros(1, 11), 1 1 1, zeros(1, 11)]; c(8) = 1; c(19) = 1;
+    end
+end
+L = sqrt( length( c ) );
+if mod(L, 1)>0, error('Init. Cond. length not a square number!'), end
 r = liferule;        % initialize Life transfer function
-k0 = kerinit(L);   % initialize kernel sub-vectors
+k = kernel(L);       % initialize convolution kernel
 ptr = zeros(1, L^2);
 imagesc( reshape(1-c, L, L) ), pause, colormap gray
-for t=1:steps
-    % evolve via BCCB convolution
-    j = 1;
-    for i=1:length(c)
-        [k, j] = kernel( k0, i, j ) ;
-        ptr(i) = k*c' ;   
-    end 
-    c = r( ptr + 1 );
+title('Press any key to continue')
+% evolve via BCCB convolution
+for t=1:steps    
+    ptr = k*c' ;   
+    disp(ptr')
+    c = c + r( ptr' + 1 );  % discretized pde representation, r(x) ~ tanh( k*cos( [x +delta(x)]/pi ) ), k >> 1
     if sum(c) == 0, disp(['All cells dead at t = ',num2str(t)]), break, end
-    if strcmp(graph, 'L'), imagesc( reshape(1-c, L, L) ), colormap gray, pause(0.1), end
-    if strcmp(graph, 'h'), imagesc( reshape(ptr, L, L) ), pause(0.1), end
-    if strcmp(graph, 'hft'), psh = abs( fft(ptr) ); imagesc( fftshift( reshape( psh, L, L) ) ), pause(0.1), end
-    if strcmp(graph, 'Lft'), psv = abs( fft(c) ); imagesc( fftshift( reshape( psv, L, L) ) ), pause(0.1),  end
+    if strcmp(graph, 'L'), imagesc( reshape(1-c, L, L) ), colormap gray, end
+    if strcmp(graph, 'h'), imagesc( reshape(ptr, L, L) ), end
+    if strcmp(graph, 'hft'), psh = abs( fft(ptr) ); imagesc( fftshift( reshape( psh, L, L) ) ), end
+    if strcmp(graph, 'Lft'), psv = abs( fft(c) ); imagesc( fftshift( reshape( psv, L, L) ) ), end
+    title(['Step = ',num2str(t) ]), colormap gray, pause(0.1)
 end
 end
