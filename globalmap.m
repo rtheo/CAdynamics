e655d3477efe49a2018e4e654af18f97a99e2b4a
Theophanes Raptis <rtheo@dat.demokritos.gr>
2016-05-30 21:04:49 +0300

    Update globalmap.m

--- globalmap.m
+++ globalmap.m
@@ -37,14 +37,3 @@ dz = z - (1:length(z));
 figure(2), plot(dz,'.'), title('deviation from linearity')
 disp(['Fixed points: ',num2str(sum(dz==0))])
 end
-
-function s = IPFBAA(s, n)
-% Inf. Prec. Fast Binary Adder Automaton replaces binary counter model
-% Complexity varies with Leading Zero and Digit-Sum
-if isempty( s ) || all(s), s = zeros( 1, n ); return, end % recycling
-if s(1), % mod 2
-    for i=1:n, s(i) = 0; if ~s(i+1); s(i+1) = 1; break; end, end
-else
-    s(1) = 1;
-end
-end
